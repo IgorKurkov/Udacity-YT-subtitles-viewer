abc2ad92d0d9e8334d2715f2aa80573f36ca06ed
kurkov87 <kurkov87@gmail.com>
2017-12-20 11:37:57 +0200

    clean up popup.js

--- popup.js
+++ popup.js
@@ -1,25 +1,14 @@
-
-
-
 document.addEventListener('DOMContentLoaded', function() {
-
-  function sendTextToDiv() { //text
+  function renderDiv() { 
     chrome.tabs.executeScript({
-       code: 'parseSubtitlesToPane(); ' //document.getElementById("txt").innerHTML = "'+text+'"; 
+       code: 'parseSubtitlesToPane();'
     }); 
   }
- 
-  
 
-  var body = document.getElementById('content');
-  var checkPageButton = document.getElementById('checkPage');
-
-  checkPageButton.addEventListener('click', function() {
+  document.getElementById('checkPage').addEventListener('click', function() {
     chrome.tabs.getSelected(null, function(tab) {
-       
-      
-       sendTextToDiv();
-    setTimeout(function(){ window.close(); }, 500);
+      renderDiv();
+      setTimeout(function(){ window.close(); }, 500);
     });
   }, false);
 }, false);
