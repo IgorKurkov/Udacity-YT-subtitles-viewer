7f714f4900a331e4990a6cc428449f6ac5580e44
Igor Kurkov <kurkov87@gmail.com>
2017-12-21 15:17:28 +0200

    fix styles for action button

--- content.js
+++ content.js
@@ -68,7 +68,7 @@ function convertSpecialChars(str) {
   str = str.replace(/&gt;/g, ">");
   str = str.replace(/&lt;/g, "<");
   str = str.replace(/&quot;/g, '"');
-  str = str.replace("&#039;", "'");
+  str = str.replace(/&#039;/g, "'");
   return str;
 }
 
